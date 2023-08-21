0b958f6f520d9d200a046be0277f33ed1875532f
EsterMel0 <estefanymelo028@gmail.com>
2023-08-21 16:51:08 -0400

    adicionando filtro de busca

--- api/serializers.py
+++ api/serializers.py
@@ -5,12 +5,4 @@ from api.models import FundoImobiliario
 class FundoImobiliarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = FundoImobiliario
-        fields = [
-            'id',
-            'codigo',
-            'setor',
-            'dividend_yield_medio_12m',
-            'vacancia_financeira',
-            'vacancia_fisica',
-            'quantidade_ativos'
-        ]
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,9 +1,17 @@
+# flake8: noqa
 from api.serializers import FundoImobiliarioSerializer
 from rest_framework import viewsets, permissions
 from api.models import FundoImobiliario
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class FundoImobiliarioViewSet(viewsets.ModelViewSet):
     queryset = FundoImobiliario.objects.all()
     serializer_class = FundoImobiliarioSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['quantidade_ativos']
+    search_fields = ['setor', 'codigo']
+
+    
